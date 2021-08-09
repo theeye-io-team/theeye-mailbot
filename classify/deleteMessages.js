48b8153d9654baf81920e3e4afa97fc0353aa7f2
facugon <facugon@theeye.io>
2021-08-09 15:39:30 -0300

    retab

--- classify/deleteMessages.js
+++ classify/deleteMessages.js
@@ -9,33 +9,33 @@ const main = module.exports = async () => {
 
   const mailBot = new MailBot(config)
 
-    const currentDate = DateTime.now().setZone(tz)
-    console.log(`CurrentDate: ${currentDate.toFormat('dd MMMM, yyyy')}`)
-  
+  const currentDate = DateTime.now().setZone(tz)
+  console.log(`CurrentDate: ${currentDate.toFormat('dd MMMM, yyyy')}`)
+
   // Filter
   // The order of the filter must be consistent with the order in the config file
   // const searchCriteria = mailBot.buildSearchCriteria([`${filter.from}`,`${filter.subject}`,`${filter.body}`])
   // const messages = await mailBot.searchMessages(searchCriteria)
 
-    // No filter, will get everything
-    const messages = await mailBot.searchMessages(['ALL'])
+  // No filter, will get everything
+  const messages = await mailBot.searchMessages(['ALL'])
 
-    const totalMessages = messages.length
-    let deletedMessages = 0
+  const totalMessages = messages.length
+  let deletedMessages = 0
 
-    for(const message of messages) {
-      const mailDate = adjustTimezone(mailBot.getDate(message))
-      const hoursSinceReceived = (currentDate - mailDate)/3600000
-      console.log(`MailDate: ${mailDate.toFormat('dd MMMM, yyyy')}`)
-      console.log(`Hours since reception: ${hoursSinceReceived}`)
-      
-      if(hoursSinceReceived >= deletionUpToHours) {
-        mailBot.deleteMessage(message)
-        deletedMessages++
-      }
+  for(const message of messages) {
+    const mailDate = adjustTimezone(mailBot.getDate(message))
+    const hoursSinceReceived = (currentDate - mailDate)/3600000
+    console.log(`MailDate: ${mailDate.toFormat('dd MMMM, yyyy')}`)
+    console.log(`Hours since reception: ${hoursSinceReceived}`)
+
+    if(hoursSinceReceived >= deletionUpToHours) {
+      mailBot.deleteMessage(message)
+      deletedMessages++
     }
+  }
 
-    return `Total messages: ${totalMessages}, Deleted Messages: ${deletedMessages}`
+  return `Total messages: ${totalMessages}, Deleted Messages: ${deletedMessages}`
 }
 
 
