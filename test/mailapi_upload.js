36686f414e875cf0f004cb06ff04559d358e580b
facugon <facugon@theeye.io>
2022-06-30 14:40:15 -0300

    test: mailapi upload/check

---
+++ test/mailapi_upload.js
@@ -0,0 +1,44 @@
+require('dotenv').config()
+
+const Helpers = require('../lib/helpers')
+const MailApi = require('../lib/api')
+const EncConfig = require('../lib/config')
+
+const main = async (filename, initialDate = null) => {
+
+  if (!filename) {
+    throw new Error('Invalid params. Filename required')
+  }
+
+  const config = EncConfig.decrypt()
+  const client = new MailApi(config)
+
+  const date = (initialDate? new Date(initialDate) : new Date())
+  const timestamp = date.getTime()
+
+  const from = `test+${timestamp}@theeye.io`
+  const subject = `test#${timestamp}`
+
+  const hashMail = Helpers.createHash(`${from}${subject}${date.toISOString()}`)
+
+  const payload = {
+    folder: config.folders.INBOX,
+    from,
+    subject,
+    reception_date: date,
+    mail_hash: hashMail,
+    attachment_filename: `${subject}.pdf`,
+    attachment_hash: hashMail,
+    attachment_renamed: `${subject}.pdf`
+  }
+
+  console.log(payload)
+  const response = await client.upload(payload, filename)
+
+  return response
+
+}
+
+main(process.argv[2], process.argv[3])
+  .then(console.log)
+  .catch(console.error)
