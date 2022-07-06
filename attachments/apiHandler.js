4e6a4aa1a8dcdfb287b89cc30fd647e105184dac
damian-theeye <damian@theeye.io>
2022-07-05 23:09:56 -0300

    feat: updated attachments mailbot

---
+++ attachments/apiHandler.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const MailApi = require('../lib/api')
+const config = require('../lib/config').decrypt()
+
+const main = module.exports = async (attachments) => {
+
+  const mailApi = new MailApi(config)
+
+  const uploadPromises = []
+
+  for (const attachment of attachments) {
+
+    const {attachment_data, ...attachment_payload} = attachment
+
+    uploadPromises.push(mailApi.checkExists(attachment_payload)
+    .then(res => {
+      if(/not found/i.test(res.body)) {
+        return 'ok'
+        return mailApi.upload(attachment_payload, attachment_data)
+      }
+    })
+    )
+  }
+
+  return Promise.allSettled(uploadPromises)
+}
+
+if (require.main === module) {
+  main().then(console.log).catch(console.error)
+}
