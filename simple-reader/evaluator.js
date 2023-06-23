526d8305106a341582a6f2e62fe6e6ec83907965
facugon <facugon@theeye.io>
2023-06-23 16:18:06 -0300

    feat: simple reader mailbot (#17)
    * feat: simple reader mailbot
    
    * simple filter using chai.assert
    
    * advance always
    
    * parameters
    
    * ident

---
+++ simple-reader/evaluator.js
@@ -0,0 +1,89 @@
+
+
+/**
+
+ en la primera que encuentra termina.
+ si llega al final continua por el flow de resumir.
+
+ soporta todas las operaciones que soporta chai
+ https://www.chaijs.com/api/assert/
+
+**/
+/**
+
+ sample rules
+
+const filters = [
+  {
+    assertions: [
+      { prop: 'to', oper: 'equal', params: ['josefina.benavidez@theeye.io', null] },
+      { prop: 'from', oper: 'match', params: [/@theeye\.io/i, null] }
+    ],
+    event_name: 'resumir'
+  },
+  {
+    assertions: [
+      { prop: 'to', oper: 'equal', params: ['support@theeye', null] },
+      { prop: 'from', oper: 'match', params: [/@theeye.io/i, null] },
+      { prop: 'subject', oper: 'match', params: [ /resum/i ] }
+    ],
+    event_name: 'resumir'
+  },
+  {
+    assertions: [
+      { prop: 'from', oper: 'match', params: [/@theeye\.io/i, null] }
+    ],
+    event_name: 'ignorar'
+  }
+]
+
+*/
+
+const { assert } = require('chai')
+const fs = require('fs')
+
+// NodeJs boilerplate
+const main = async (args) => {
+
+  const filters = fs.readFileSync(process.env.FILTERS_FILE_PATH)
+  
+  const params = JSON.parse(args[0])
+  
+  let found = false
+  for (let index = 0; index < filters.length && !found; index++) {
+    try {
+      const condition = filters[index]
+      satisfyAllConditions(condition, params)
+      found = condition
+    } catch (err) {
+      if (err.name === 'AssertionError') {
+        console.log(err.message)
+      } else {
+        throw err
+      }
+    }
+  }
+
+  const result = {
+    data: params, 
+    event_name: found?.event_name || process.env.DEFAULT_EVENT_NAME
+  }
+
+  // add your code here.
+
+  return result
+}
+
+// en cuanto falla sale por exception
+const satisfyAllConditions = (condition, params) => {
+  for (let index = 0; index < condition.assertions.length; index++) {
+    const eval = condition.assertions[index]
+    assert.property(params, eval.prop)
+    assert[ eval.oper ](params[eval.prop], ...eval.params)
+  }
+  return true
+}
+
+if (require.main === module) {
+  main(process.argv.slice(2)).then(console.log).catch(console.error)
+}
---
+++ simple-reader/index.js
@@ -0,0 +1,43 @@
+
+const MailBot = require('../lib/mailbot')
+const config = require('../lib/config').decrypt()
+
+const main = module.exports = async (params) => {
+
+  const mailBot = new MailBot(config)
+  await mailBot.connect()
+
+  const messages = await mailBot.searchMessages({})
+
+  let result
+  if (messages.length > 0) {
+    const message = messages[0]
+    await message.getContent()
+
+    let body
+    if (message.body) {
+      body = Buffer.from(message.body).toString('base64')
+    }
+
+    const data = [
+      {
+        to: message.data.to.text,
+        subject: message.subject,
+        from: message.from,
+        body
+      }
+    ]
+    await message.move()
+
+    result = { event_name: 'message', data }
+  } else {
+    result = { event_name: 'end', data: [ 'no more messages' ] }
+  }
+
+  await mailBot.closeConnection()
+  return result
+}
+
+if (require.main === module) {
+  main(process.argv[2]).then(console.log).catch(console.error)
+}
